e96178dca6679af84b3dfa9a6f16b21019ceea5e
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2023-09-18 20:32:14 +0600

    refine contact-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import {HeroPage} from "./pages/HeroPage";
-import {Route, Routes} from "react-router-dom";
-import {AboutPage} from "./pages/AboutPage";
-import {ContactPage} from "./pages/ContactPage";
-import {ProjectsPage} from "./pages/ProjectsPage";
-import {Navbar} from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { HeroPage } from "./pages/HeroPage";
+import { AboutPage } from "./pages/AboutPage";
+import { ProjectsPage } from "./pages/ProjectsPage";
+import { ContactPage } from "./pages/ContactPage";
 
 import galaxy_bg from "../src/assets/galaxy_bg.jpg";
 
 function App() {
-    return (
-        <div style={{backgroundImage: `url('${galaxy_bg}')`}}
-             className='bg-cover bg-gradient-to-r from-gray-950 via-gray-900 to-gray-950 text-white min-h-screen min-w-full overflow-x-hidden overflow-y-hidden px-5 lg:px-0'>
-            <Navbar/>
-            <Routes>
-                <Route path='/' element={<HeroPage/>}/>
-                <Route path='about' element={<AboutPage/>}/>
-                <Route path='projects' element={<ProjectsPage/>}/>
-                <Route path='contact' element={<ContactPage/>}/>
-            </Routes>
-        </div>
-    );
+  return (
+    <div style={{ backgroundImage: `url('${galaxy_bg}')` }} className="bg-cover bg-gradient-to-r from-gray-950 via-gray-900 to-gray-950 text-white min-h-screen min-w-full overflow-x-hidden overflow-y-hidden px-5 lg:px-0">
+      <Navbar />
+      
+      <Routes>
+        <Route path="/" element={<HeroPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="projects" element={<ProjectsPage />} />
+        <Route path="contact" element={<ContactPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
